fc266722c36440cce277c656ea8f94839f25610b
Adam Argyle <argyle@google.com>
2020-10-05 14:11:33 -0700

    add worklet

---
+++ static-gradient.worklet.js
@@ -0,0 +1,52 @@
+const paint_name = 'static-gradient'
+const offset = .01
+const start  = 1 + offset
+const sizekeys = {
+  small:  2,
+  medium: 8,
+  large:  16,
+}
+
+registerPaint(paint_name, class StaticGradient {
+  static get inputArguments() {
+    return [
+      'to-top | to-right | to-bottom | to-left',
+      '<color>',
+      '<number> | small | medium | large',
+    ]
+  }
+
+  paint(ctx, bounds, props, args) {
+    const { width:w, height:h } = bounds
+
+    let [ direction, color, size ] = args.map(arg => arg?.toString() || undefined)
+
+    if (isNaN(parseInt(size)))
+      size = sizekeys[size]
+
+    const end = 1 / size - offset
+
+    for (let y = 0; y < h; y++) {
+      for (let x = 0; x < w; x++) {
+
+        switch (direction.toString()) {
+          case 'to-top':
+            if (Math.random() < (start - y / (h*end))) continue
+            break;
+          case 'to-right':
+            if (Math.random() < (start - x / (w*end))) continue
+            break;
+          case 'to-bottom':
+            if (Math.random() > (start - y / (h*end))) continue
+            break;
+          case 'to-left':
+            if (Math.random() > (start - x / (w*end))) continue
+            break;
+        }
+
+        ctx.fillStyle = color
+        ctx.fillRect(x * size, y * size, size, size)
+      }
+    }
+  }
+})
