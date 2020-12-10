ef8a7adfe98ff267cc9b281bae2b30597dc9d881
Adam Argyle <argyle@google.com>
2020-12-10 09:05:28 -0800

    adds messaging for support

--- app/js/index.js
+++ app/js/index.js
@@ -8,3 +8,10 @@
 document.querySelector('h1').addEventListener('click', e => {
   e.target.classList.toggle('paused')
 })
+
+var div = document.createElement('div')
+div.style.background = 'paint(static-gradient, 1)'
+if (!/static-gradient/.test(div.style.background)) {
+  console.log('custom paint arguments not supported')
+  document.querySelector('small').innerHTML = 'This worklet requires <a href="chrome://flags/#enable-experimental-web-platform-features">#enable-experimental-web-platform-features</a> to be enabled'
+}
